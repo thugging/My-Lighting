6850e6d9a374e1d32a3d7c65fe4e3c6362f2ba74
thugging <93628674+thugging@users.noreply.github.com>
2022-11-09 12:33:00 -0600

    Add files via upload

---
+++ Lighting.lua
@@ -0,0 +1,12 @@
+game.Lighting.ClockTime = 0
+game.Lighting.FogEnd = 2000
+game.Lighting.FogColor = 0,0,255
+game.Lighting.FogEnd = 0
+game.Lighting.OutdoorAmbient = 0,0,255
+game.Lighting.ShadowColor = 0,0,255
+game.Lighting.ShadowSoftness = 0.3
+game.Lighting.Ambient = 0,0,255
+game.Lighting.ColorShift_Bottom = 0,0,255
+game.Lighting.ColorShift_Top = 0,0,255
+game.Lighting.Brightness = 9.1
+game.Lighting.EnvironmentDiffuseScale = 0.5
