0f22e663b69705da349d8936a832b04c79e2e99f
thugging <93628674+thugging@users.noreply.github.com>
2022-11-09 13:14:55 -0600

    Update Lighting.lua

--- Lighting.lua
+++ Lighting.lua
@@ -1,12 +1,15 @@
+--lights.lua 
+---------------------------------------------------------------------
 game.Lighting.ClockTime = 0
 game.Lighting.FogEnd = 2000
-game.Lighting.FogColor = 0,0,255
+game.Lighting.FogColor = Color3.fromRGB(0, 0, 255)
 game.Lighting.FogEnd = 0
-game.Lighting.OutdoorAmbient = 0,0,255
-game.Lighting.ShadowColor = 0,0,255
+game.Lighting.OutdoorAmbient = Color3.fromRGB(0, 0, 255)
+game.Lighting.ShadowColor = Color3.fromRGB(0, 0, 255)
 game.Lighting.ShadowSoftness = 0.3
-game.Lighting.Ambient = 0,0,255
-game.Lighting.ColorShift_Bottom = 0,0,255
-game.Lighting.ColorShift_Top = 0,0,255
+game.Lighting.Ambient = Color3.fromRGB(0, 0, 255)
+game.Lighting.ColorShift_Bottom = Color3.fromRGB(0, 0, 255)
+game.Lighting.ColorShift_Top = Color3.fromRGB(0, 0, 255)
 game.Lighting.Brightness = 9.1
 game.Lighting.EnvironmentDiffuseScale = 0.5
+game.Workspace.Camera.FieldOfView = 120
